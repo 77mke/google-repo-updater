6f1eebfec650efa8705ee14855225f7c1420db35
Matt Knickelbine <matt@77creative.io>
2020-10-01 16:40:16 -0500

    added bash support

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,114 +4,114 @@ import { IAppConfig, IPubSubNotification, IRepoConfig } from "../interfaces";
 import { PubSubService } from "./services/pubSub.service";
 
 export class App {
-  private pubSub: PubSubService;
+    private pubSub: PubSubService;
 
-  constructor(private config: IAppConfig) {
-    this.init();
+    constructor(private config: IAppConfig) {
+        this.init();
 
-    // Start the app
-    this.startApp();
-  }
+        // Start the app
+        this.startApp();
+    }
 
-  private startApp() {
-    // Start the subscription
-    this.subscribeToPubSub();
+    private startApp() {
+        // Start the subscription
+        this.subscribeToPubSub();
 
-    // Setup exit handler
-    process.on("SIGINT", async () => {
-      console.log("Shutting down...");
+        // Setup exit handler
+        process.on("SIGINT", async () => {
+            console.log("Shutting down...");
 
-      // Unsubscribe from pubsub
-      await this.pubSub.unsubscribe();
+            // Unsubscribe from pubsub
+            await this.pubSub.unsubscribe();
 
-      // Exit
-      process.exit();
-    });
+            // Exit
+            process.exit();
+        });
 
-    // ... that's all folks
-  }
+        // ... that's all folks
+    }
 
-  private subscribeToPubSub(): void {
-    // Get a new session id
-    const sessionId: string = this.createSessionID();
+    private subscribeToPubSub(): void {
+        // Get a new session id
+        const sessionId: string = this.createSessionID();
 
-    console.log("Starting subscription...");
+        console.log("Starting subscription...");
 
-    // Setup subscription
-    this.pubSub.subscribe(
-      this.config.topic,
-      sessionId,
-      (data: IPubSubNotification) => {
-        console.log("GOT DATA");
-        // Loop through all of the configs
-        for (let config of this.config.configs) {
-          // Validate the repository
-          if (!this.isCorrectRepository(data, config)) {
-            continue;
-          }
+        // Setup subscription
+        this.pubSub.subscribe(
+            this.config.topic,
+            sessionId,
+            (data: IPubSubNotification) => {
+                console.log("GOT DATA");
+                // Loop through all of the configs
+                for (let config of this.config.configs) {
+                    // Validate the repository
+                    if (!this.isCorrectRepository(data, config)) {
+                        continue;
+                    }
 
-          // Validate the branch
-          if (!this.isCorrectBranch(data, config)) {
-            continue;
-          }
+                    // Validate the branch
+                    if (!this.isCorrectBranch(data, config)) {
+                        continue;
+                    }
 
-          console.log("Executing script for config");
-          console.log(config);
+                    console.log("Executing script for config");
+                    console.log(config);
 
-          // Execute the script
-          this.executeScript(config);
-        }
-      }
-    );
-  }
+                    // Execute the script
+                    this.executeScript(config);
+                }
+            }
+        );
+    }
 
-  private isCorrectRepository(
-    data: IPubSubNotification,
-    config: IRepoConfig
-  ): boolean {
-    console.log(data.name + " vs. " + config.repository);
-    return data.name === config.repository;
-  }
+    private isCorrectRepository(
+        data: IPubSubNotification,
+        config: IRepoConfig
+    ): boolean {
+        console.log(data.name + " vs. " + config.repository);
+        return data.name === config.repository;
+    }
 
-  private isCorrectBranch(
-    data: IPubSubNotification,
-    config: IRepoConfig
-  ): boolean {
-    // Cast the ref updates to an array
-    const refs = Object.keys(data.refUpdateEvent.refUpdates);
+    private isCorrectBranch(
+        data: IPubSubNotification,
+        config: IRepoConfig
+    ): boolean {
+        // Cast the ref updates to an array
+        const refs = Object.keys(data.refUpdateEvent.refUpdates);
 
-    // Check if the branch name is in the array
-    return refs.find(r => r === config.branch) ? true : false;
-  }
+        // Check if the branch name is in the array
+        return refs.find((r) => r === config.branch) ? true : false;
+    }
 
-  private executeScript(config: IRepoConfig): void {
-    // Execute the attached script
-    child_process.exec(
-      `sh ${config.shellScript}`,
-      (error: Error, stdout: string, stderr: string) => {
-        if (error) {
-          console.log("Eror running shell script");
-          console.log(error);
-          return;
-        }
+    private executeScript(config: IRepoConfig): void {
+        // Execute the attached script
+        child_process.exec(
+            `bash ${config.shellScript}`,
+            (error: Error, stdout: string, stderr: string) => {
+                if (error) {
+                    console.log("Eror running shell script");
+                    console.log(error);
+                    return;
+                }
 
-        console.log(stdout);
-        console.log(stderr);
-      }
-    );
-  }
+                console.log(stdout);
+                console.log(stderr);
+            }
+        );
+    }
 
-  private createSessionID(): string {
-    // Use the uuid/v4 library to create unique ids
-    return uuid();
-  }
+    private createSessionID(): string {
+        // Use the uuid/v4 library to create unique ids
+        return uuid();
+    }
 
-  private init() {
-    // Build required services
-    this.pubSub = new PubSubService(
-      this.config.keyFile,
-      this.config.projectId,
-      this.config.subId
-    );
-  }
+    private init() {
+        // Build required services
+        this.pubSub = new PubSubService(
+            this.config.keyFile,
+            this.config.projectId,
+            this.config.subId
+        );
+    }
 }
